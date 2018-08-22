e248395b9d3d9dbad0e581d873f1d8dfe90b633a
sensei_88 <agbottahw@gmail.com>
2018-08-22 13:01:46 -0700

    folder restructure

--- build/src-sw.js
+++
@@ -1,6 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
-
-//Custom adjustments
-console.log('our custom adjustments');
-
-workbox.precaching.precacheAndRoute([]);
--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "globDirectory": "build/",
-  "globPatterns": [
-    "**/*.{css,svg,html,js}"
-  ],
-  "swDest": "build\\sw.js",
-  "swSrc": "build\\src-sw.js"
-};
